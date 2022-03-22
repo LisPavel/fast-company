dd9774c00c39fe8c6e9189bcead78344665fed11
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-03-22 12:29:41 +0300

    added title

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import API from "../api";
 
 const Users = () => {
-    return <h1>Users</h1>;
+    const [users, setUsers] = useState(API.users.fetchAll());
+
+    const formatPersonsTxt = (users) => {
+        return `человек${users.length >= 2 && users.length <= 4 ? "а" : ""}`;
+    };
+
+    const getTitleText = (users) => {
+        return users.length > 0
+            ? `${users.length} ${formatPersonsTxt(
+                  users,
+              )} тусанет с тобой сегодня`
+            : "Никто с тобой не тусанет";
+    };
+
+    const renderTitle = () => {
+        const titleTxt = getTitleText(users);
+
+        const classes = `badge bg-${users.length > 0 ? "primary" : "danger"}`;
+        return (
+            <h2>
+                <span className={classes}>{titleTxt}</span>
+            </h2>
+        );
+    };
+    return <>{renderTitle()}</>;
 };
 
 export default Users;
