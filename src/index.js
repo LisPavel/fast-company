c701b87bbcd67509f330167e84abcfc6c8066d7c
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-04-01 10:32:16 +0300

    added icons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from "./app";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import reportWebVitals from "./reportWebVitals";
 
