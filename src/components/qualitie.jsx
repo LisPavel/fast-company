6945d10837b9479804fbee2ca660e71e253b73c5
Pavel Lisitsyn <pavellisitsyn69@gmail.com>
2022-04-07 23:16:35 +0300

    added prop types

--- src/components/qualitie.jsx
+++ src/components/qualitie.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Qualitie = (props) => {
   const classes = `badge bg-${props.color} m-1`;
 
   return <span className={classes}>{props.name}</span>;
+};
+
+Qualitie.propTypes = {
+  color: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default Qualitie;
