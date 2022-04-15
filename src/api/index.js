1e20527921c52eab1b5f206a3c4f2d8924cdf09a
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-04-15 17:18:44 +0300

    redo fake api
    move professions to other file

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,8 @@
 import * as users from "./fake.api/user.api";
+import professionsApi from "./fake.api/professions.api";
+
 const API = {
   users,
+  professions: professionsApi,
 };
 export default API;
