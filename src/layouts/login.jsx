77709bba89808ca46313b0460d54728441b593f3
Pavel Lisitsyn <pavellisitsyn69@gmail.com>
2022-05-03 14:16:59 +0300

    added form

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LogIn = () => {
-  return <h1>Login</h1>;
+  const [data, setData] = useState({ email: "", password: "" });
+  const handleChange = ({ target }) => {
+    setData((prevState) => ({
+      ...prevState,
+      [target.name]: target.value,
+    }));
+    // setEmail(ev.target.value);
+  };
+  return (
+    <form action="">
+      <div>
+        <input
+          type="text"
+          id="email"
+          value={data.email}
+          onChange={handleChange}
+          name="email"
+        />
+        <label htmlFor="email">Email</label>
+      </div>
+      <div>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={data.password}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+      </div>
+    </form>
+  );
 };
 
 export default LogIn;
