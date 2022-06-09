a85d28f29b1850b58be6ce4bf3e3a5f1b795bfd5
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-06-09 13:09:58 +0300

    save user data

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,6 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
+import userService from "../services/userService";
+import { toast } from "react-toastify";
 // import userService from "../services/userService";
 // import { toast } from "react-toastify";
 
@@ -11,19 +13,63 @@ export const useAuth = () => {
     return useContext(AuthContext);
 };
 
+const TOKEN_KEY = "jwt-token";
+const REFRESH_KEY = "jwt-refresh-token";
+const EXPIRES_KEY = "jwt-expires";
+
 export const AuthProvider = ({ children }) => {
+    const [currentUser, setUser] = useState({});
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (error) {
+            toast.error(error);
+            setError(null);
+        }
+    }, [error]);
+
+    function setTokens({ refreshToken, idToken, expiresIn = 3600 }) {
+        const expiresDate = new Date().getTime() + expiresIn * 1000;
+        localStorage.setItem(TOKEN_KEY, idToken);
+        localStorage.setItem(REFRESH_KEY, refreshToken);
+        localStorage.setItem(EXPIRES_KEY, expiresDate);
+    }
+
     const signUp = async ({ email, password, ...rest }) => {
         const key = "AIzaSyDH5J236OxkC-LhOKwi4nD8yG1uKt6gL5A";
         const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
-        const { data } = await httpAuth.post(url, {
-            email,
-            password,
-            returnSecureKey: true,
-        });
-        console.log(data);
+        try {
+            const { data } = await httpAuth.post(url, {
+                email,
+                password,
+                returnSecureKey: true,
+            });
+            setTokens(data);
+            await createUser({ ...rest, email, _id: data.localId });
+            console.log(data);
+        } catch (error) {
+            errorCatcher(error);
+        }
     };
+
+    async function createUser(data) {
+        try {
+            const { content } = await userService.create(data);
+            setUser(content);
+            console.log(content);
+        } catch (error) {
+            errorCatcher(error);
+        }
+    }
+
+    function errorCatcher(error) {
+        console.log(error);
+        const { message } = error.response.data;
+        setError(message);
+    }
+
     return (
-        <AuthContext.Provider value={{ signUp }}>
+        <AuthContext.Provider value={{ signUp, currentUser }}>
             {children}
         </AuthContext.Provider>
     );
