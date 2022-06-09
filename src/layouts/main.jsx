85647dcec8019d6686e8aa39bcfbabc09c5a2dda
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-06-09 11:08:02 +0300

    upload mock data

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import useMockData from "../utils/mockData";
 
 const Main = () => {
-    return <h1>Main</h1>;
+    const { initialize, status, error, progress } = useMockData();
+    const handleClick = () => {
+        console.log("click");
+        initialize();
+    };
+    return (
+        <div className="container mt-5">
+            <h1>Main</h1>
+            <h3>Init data in fireBase</h3>
+            <button className="btn btn-primary" onClick={handleClick}>
+                INIT!
+            </button>
+            <ul>
+                <li>Status : {status}</li>
+                <li>Progress: {progress}%</li>
+                {error && <li>Error</li>}
+            </ul>
+        </div>
+    );
 };
 
 export default Main;
